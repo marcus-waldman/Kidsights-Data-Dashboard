0371adb0440c7b15071edf04ab527dab3f1f8552
marcus.waldman@cuanschutz.edu <marcus.waldman@unmc.edu>
2025-07-26 00:47:34 -0600

    tweak example for shinychat

---
+++ utils/demo-shinychat.R
@@ -0,0 +1,23 @@
+if(FALSE){
+  library(shiny)
+  library(shinychat)
+  library(ellmer)
+  
+ # Sys.setenv(ANTHR0PIC_AdI_K3Y = "")
+  ui <- bslib::page_fluid(
+    chat_ui("chat", fill = T)
+  )
+  
+  server <- function(input, output, session) {
+    
+    observeEvent(input$chat_user_input, {
+      print(input$chat_user_input)
+      chat <- ellmer::chat_anthropic(system_prompt = input$chat_user_input)
+      stream <- chat$stream_async(input$chat_user_input, tool_mode = "sequential")
+      shinychat::chat_append("chat", stream)
+    })
+  }
+  
+  shinyApp(ui, server)
+}
+
